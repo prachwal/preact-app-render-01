2bc0d635f8429d5fdae3c3bf006ef5715928e878
Przemyslaw Rachwlal <przemyslaw.rachwal@gmail.com>
2025-12-05 16:29:44 +0100

    Add Fastify as dev dependency, update API script

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
-const fastify = require('fastify')({ logger: true });
+import fastify from 'fastify';
 
-fastify.get('/api/hello', async (request, reply) => {
+const app = fastify({ logger: true });
+
+app.get('/api/hello', async (request, reply) => {
   return { message: 'Hello from Render with Fastify!' };
 });
 
 const port = process.env.PORT || 3000;
-fastify.listen({ port, host: '0.0.0.0' }, (err, address) => {
+app.listen({ port, host: '0.0.0.0' }, (err, address) => {
   if (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
-  fastify.log.info(`Server listening on ${address}`);
+  app.log.info(`Server listening on ${address}`);
 });
